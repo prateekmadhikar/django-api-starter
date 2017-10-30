1c0f6e2315e46971bfb9b939d6f3600a4470ae61
Prateek Madhikar <prateekmadhikar@yahoo.com>
2017-10-30 19:22:33 -0400

    implement list routes

--- app/routes/list_routes.py
+++ app/routes/list_routes.py
@@ -1,4 +1,7 @@
-from flask import Blueprint
+import httplib
+import json
+
+from flask import Blueprint, Response
 
 from app.services.list_service import ListService
 
@@ -11,28 +14,64 @@ def get_all_lists():
     lists = list_service.get_all_lists()
 
     response = {
-        'lists': [l.to_json() for l in lists]
+        'lists': [l.to_json_serializable_dict() for l in lists]
     }
 
-    return response
+    return Response(response=json.dumps(response),
+                    status=httplib.OK,
+                    mimetype='application/json')
 
 
 @list_blueprint.route('/', methods=['POST'])
 def create_list():
+    req_json = request.json
 
-    pass
+    list = list_service.create_list(req_json['name'])
+
+    return Response(response=json.dumps(list.to_json_serializable_dict()),
+                    status=httplib.CREATED,
+                    mimetype='application/json')
 
 
 @list_blueprint.route('/{list_id}', methods=['GET'])
 def get_list(list_id):
-    pass
+    list = list_service.get_list(list_id)
+
+    if list:
+        return Response(response=json.dumps(list.to_json_serializable_dict()),
+                        status=httplib.OK,
+                        mimetype='application/json')
+
+    return Response(response=json.dumps({'error': 'invalid list_id'}),
+                    status=httplib.NOT_FOUND,
+                    mimetype='application/json')
 
 
 @list_blueprint.route('/{list_id}', methods=['PUT'])
 def update_list(list_id):
-    pass
+    req_json = request.json
+
+    list = list_service.update_list(list_id,
+                                    name=req_json.get('name', None))
+
+    if list:
+        return Response(response=json.dumps(list.to_json_serializable_dict()),
+                        status=httplib.OK,
+                        mimetype='application/json')
+
+    return Response(response=json.dumps({'error': 'invalid list_id'}),
+                    status=httplib.NOT_FOUND,
+                    mimetype='application/json')
 
 
 @list_blueprint.route('/{list_id}', methods=['DELETE'])
 def delete_list(list_id):
-    pass
+    is_deleted = list_service.delete_list(list_id)
+
+    if is_deleted:
+        return Response(status=httplib.OK,
+                        mimetype='application/json')
+
+    return Response(response=json.dumps({'error': 'invalid list_id'}),
+                    status=httplib.NOT_FOUND,
+                    mimetype='application/json')
